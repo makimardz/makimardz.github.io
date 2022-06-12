6a24ed91190916ecd5a54e36002715301ca7a531
makimardz <makimardz@gmail.com>
2022-06-12 12:34:51 +0800

    v4

--- script/modify.js
+++ script/modify.js
@@ -1,2 +1,9 @@
-alert(document.lastModified);
-// returns: Tuesday, December 16, 2017 11:09:42
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: "numeric", minute: "numeric", second: "numeric"};
+const today = new Date().toLocaleDateString('en-UK', options);
+
+document.querySelector('#lastUpdated').textContent = `Last Updated: ${document.lastModified}`;
+
+document.querySelector('#copyRight').textContent = `\u00A9 ${year} | Landon Carter | Utah`;
